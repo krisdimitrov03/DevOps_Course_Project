6e82c90a09e6af1967fb659c10370fcb8198c47e
Kristian Dimitrov <krisitoo10@gmail.com>
2025-02-07 22:30:23 +0200

    Add project files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+@app.route("/hostname")
+def get_hostname():
+    return socket.gethostname()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
